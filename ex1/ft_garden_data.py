1cf5da6f1f43b6371cde5c16ce5a587f20418f10
Ptitchat <pau.roy.narvaez@gmail.com>
2026-07-30 09:00:34 +0200

    Working on classes

---
+++ ex1/ft_garden_data.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+class Plant:
+    def __init__(self, name: str, height: int, age: int) -> None:
+        self.name = name
+        self.height = height
+        self.age = age
+
+    def print_plant(self) -> None:
+        print(
+            f"{self.name.capitalize()}:",
+            f"{self.height}cm,",
+            f"{self.age} days old"
+        )
+
+
+if __name__ == "__main__":
+    plant = Plant("Rose", 25, 30)
+    plant.print_plant()
+    plant = Plant("Sunflower", 80, 45)
+    plant.print_plant()
+    plant = Plant("Cactus", 15, 120)
+    plant.print_plant()
